faeedf8790bf5727d58cd0cd82638b6f945ee2af
Sergey Korchalovsky <korhalovsky@gmail.com>
2020-12-18 20:12:25 +0300

    Added the ability to change the interval for sending messages

--- avito_parser.py
+++ avito_parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import os.path
 
 HOST = 'https://www.avito.ru/'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
@@ -12,7 +13,7 @@ def get_content(url):
     if html.status_code == 200:
         soup = BeautifulSoup(html.text, 'html.parser')
         items = soup.find_all('div', class_='iva-item-list-2_PpT')
-        data = read_db() # Читаем базу
+        data = read_db()  # Читаем базу
         products = []
         for item in items:
             image = item.find('ul', class_='photo-slider-list-3Zt1Z').find('li')
@@ -34,20 +35,13 @@ def get_content(url):
             })
             if len(products) == 5:
                 break
-        record_to_db(products)
+        write_new_item_to_db(products)
         return products
     else:
         print('Error')
 
 
-def read_db():
-    with open("db.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-        jsonFile.close()
-    return data
-
-
-def record_to_db(new_item):
+def write_new_item_to_db(new_item):
     new_id = [item["id"] for item in new_item]
     data = read_db()
     with open("db.json", "w") as jsonFile:
@@ -55,5 +49,26 @@ def record_to_db(new_item):
         json.dump(data, jsonFile, indent=4)
         jsonFile.close()
 
+
+def read_db():
+    if os.path.exists('db.json'):
+        with open("db.json", "r") as jsonFile:
+            data = json.load(jsonFile)
+            jsonFile.close()
+    else:
+        data = {
+            "ID": "",
+            "URL": "",
+            "interval": 300,
+            "products": []
+        }
+        write_db(data)
+    return data
+
+
+def write_db(data):
+    with open("db.json", "w") as jsonFile:
+        json.dump(data, jsonFile, indent=4)
+        jsonFile.close()
 
 # print(get_content('https://www.avito.ru/rostov-na-donu/audio_i_video?cd=1&s=104'))
--- bot.py
+++ bot.py
@@ -1,34 +1,60 @@
-import config
-import logging
 from aiogram import Bot, Dispatcher, executor, types
-import asyncio
-from avito_parser import get_content
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from avito_parser import get_content, read_db, write_db
 from urllib.parse import urlparse
+import logging
+import asyncio
+import config
 import json
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-with open("db.json", "r") as jsonFile:
-    data = json.load(jsonFile)
-    jsonFile.close()
+data = read_db()
+
+
+class Dialog(StatesGroup):
+    waiting_for_interval_time = State()
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\n"
-                        "Отправь мне ссылку на подборку товаров авито и я буду присылать тебе новые объявления!")
+                        "Отправь мне ссылку на подборку товаров авито и я каждые 5 минут буду проверять "
+                        "новые объявления!")
+
+
+@dp.message_handler(commands=['interval'])
+async def process_interval_command(message: types.Message):
+    await message.answer("С каким минутным интервалом хочешь получать новые объявления? (По умолчанию - 5 минут)")
+    await Dialog.waiting_for_interval_time.set()
+
+
+@dp.message_handler(state=Dialog.waiting_for_interval_time)
+async def interval(message: types.Message, state: FSMContext):
+    time = message.text
+    json_data = read_db()
+    json_data["interval"] = int(time) * 60
+    write_db(json_data)
+    await message.answer(f"Хорошо, теперь объявленя будут присылаться с интервалом в {time} минут")
+    await state.finish()
 
 
 @dp.message_handler(commands=['stop'])
 async def process_start_command(message: types.Message):
     data["URL"] = ""
-    with open("db.json", "w") as file:
-        json.dump(data, file, indent=4)
-        file.close()
+    write_db(data)
     await message.reply("Поиск товаров по этой ссылке остановлен.\n Отправь новую ссылку!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Отправь команду /stop, если хочешь остановить получения объявлений.\n"
+                        "Отправь команду /interval, если хочешь изменить интервал получения новых объявлений")
 
 
 @dp.message_handler()
@@ -38,9 +64,7 @@ async def start(messsage: types.Message):
     if urlparse(messsage.text).netloc == 'www.avito.ru':
         data["URL"] = str(messsage.text)
         data["products"] = []
-        with open("db.json", "w") as file:
-            json.dump(data, file, indent=4)
-            file.close()
+        write_db(data)
         await post_send()
     else:
         await bot.send_message(data['ID'], 'Неверный запрос, укажите ссылку на товары авито')
@@ -58,10 +82,11 @@ async def post_send():
                     await bot.send_photo(data['ID'], post['image'],
                                          caption=post['title'] + "\n" + "Цена: " + post['price']
                                                  + "\n" + post['geo'] + "\n\n" + post['link'])
+                    await asyncio.sleep(3)
         except TypeError:
             await bot.send_message(data['ID'], 'Некорректная ссылка')
             break
-        await asyncio.sleep(10)
+        await asyncio.sleep(data['interval'])
 
 
 if __name__ == '__main__':
